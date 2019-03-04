2cd7667a574b19a6bd6081b532c08b7b9cf5a83e
frederik <s185018@student.dtu.dk>
2019-03-04 20:19:36 +0100

    alt er indsat

--- src/main/java/dal/UserDAOImpls185018.java
+++ src/main/java/dal/UserDAOImpls185018.java
@@ -3,11 +3,12 @@ package dal;
 import dto.UserDTO;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-//TODO Rename class
+
 public class UserDAOImpls185018 implements IUserDAO {
-    //TODO Make a connection to the database
+
     private Connection createConnection() throws DALException {
         try {
             return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s185018?"
@@ -47,22 +48,70 @@ public class UserDAOImpls185018 implements IUserDAO {
 
     @Override
     public List<UserDTO> getUserList() throws DALException {
-        //TODO Implement this
-        return null;
+        List<UserDTO> userList = new ArrayList<UserDTO>();
+        Connection connection = createConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT UserID FROM Aflevering1");
+            while (resultSet.next()){
+                int ID = resultSet.getInt("UserID"); UserDTO user = getUser(ID);
+                userList.add(user);
+            }
+            connection.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return userList;
+
     }
 
     @Override
     public void createUser(UserDTO user) throws DALException {
-        //TODO Implement this
+        Connection connection = createConnection();
+        int id = 1;
+        String name = "Ny bruger";
+        String ini = "NB";
+        String roles = "Brugerrole";
+        try {
+            connection.prepareStatement("INSERT INTO aflevering1(UserID, Username, Initials, Roles) values(" + id +
+                    ", " + name + ", " + ini + ", " + roles + ");");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void updateUser(UserDTO user) throws DALException {
-        //TODO Implement this
+        Connection connection = createConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE aflevering_1 SET Username = ? WHERE UserID = ?");
+            preparedStatement.setString(1, user.getIni());
+            preparedStatement.setInt(2, user.getUserId());
+
+            preparedStatement = connection.prepareStatement("UPDATE aflevering_1 SET Initials = ? WHERE UserID = ?");
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setInt(2, user.getUserId());
+
+            preparedStatement = connection.prepareStatement("UPDATE aflevering_1 SET Roles = ? WHERE UserID = ?");
+            preparedStatement.setString(1, String.valueOf(user.getRoles()));
+            preparedStatement.setInt(2, user.getUserId());
+
+            connection.close();
+        }catch (SQLException e){
+            throw new DALException(e.getMessage());
+        }
     }
 
     @Override
     public void deleteUser(int userId) throws DALException {
-        //TODO Implement this
+        Connection connection = createConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM aflevering_1 WHERE UserID = " + userId);
+            preparedStatement.executeUpdate();
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage());
+        }
     }
 }
